8279c59a14a05bcfae93e765ec44dcaa53b5ec44
RauliYoung <tristan.ellenberg@gmail.com>
2023-09-19 18:49:21 +0300

    added root for project

---
+++ mavenTest/src/main/java/com/maven/maventest/MavenTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.maven.maventest;
+
+/**
+ *
+ * @author rauliyoung
+ */
+public class MavenTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
